e5a3ba6897286c8843f3c6d7c167315c25abf623
chen <3217855084@qq.com>
2021-02-11 21:54:51 -0500

    Create test_config_options.rb

---
+++ my_own_test/test_config_options.rb
@@ -0,0 +1,15 @@
+RACKUP = {
+  :daemonize => false,
+  :host => "127.0.0.1",
+  :port => "8080",
+  :set_listener => false,
+  :options => { :listeners => [] }
+}
+rackup_opts = RACKUP
+options = rackup_opts[:options]
+options[:config_file] = "config_testest"
+puts options[:config_file]
+puts rackup_opts[:host]
+puts rackup_opts[:test]
+rackup_opts[:test] = "settest"
+puts rackup_opts[:test]
